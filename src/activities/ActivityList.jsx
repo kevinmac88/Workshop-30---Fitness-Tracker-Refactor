5e4674d168ad4ed29d045a3b84a62b50de12966f
Lester Lee <contact@lester-lee.com>
2025-09-19 10:44:24 -0400

    Show activity delete button to logged-in user

--- src/activities/ActivityList.jsx
+++ src/activities/ActivityList.jsx
@@ -1,9 +1,22 @@
+import { useAuth } from "../auth/AuthContext";
+
 export default function ActivityList({ activities }) {
   return (
     <ul>
       {activities.map((activity) => (
-        <li key={activity.id}>{activity.name}</li>
+        <ActivityListItem key={activity.id} activity={activity} />
       ))}
     </ul>
+  );
+}
+
+function ActivityListItem({ activity }) {
+  const { token } = useAuth();
+
+  return (
+    <li>
+      <p>{activity.name}</p>
+      {token && <button>Delete</button>}
+    </li>
   );
 }
